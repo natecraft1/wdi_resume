be87d1e80cb03a0c42005cfc1115df763cc3d266
Nathan Glass <nglass1988@gmail.com>
2013-06-26 00:34:18 -0700

    all data input working

--- js/wtftest.js
+++ js/wtftest.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 //perform all those actions on the nth element}
 	
 
-	$('#p2 span').mouseenter(function(event){
+	$('#p2 span').click(function(event){
 	//prevents the event from taking us to the top of the page
 	event.preventDefault();
 	//if the link has class 'active' do nothing, otherwise remove class 'hidden' and add class 
@@ -70,12 +70,13 @@ $(document).ready(function() {
 			var first = response.responseJSON.name_first
 			var last = response.responseJSON.name_last;
 			var fullName = first + " " + last;
-			$('.one header').html(fullName);
+			$('.body6 h1').html(fullName);
+			$('.topname p').html(fullName);
 			var website = response.responseJSON.website;
 			var twitter = response.responseJSON.twitter;
 			var linkedin = response.responseJSON.linked_in;
 			var webtwitin = website + '<br><hr>' + twitter + '<br><hr>' + linkedin;
-			$('.body1').html(webtwitin);
+			$('.body6 p').html(webtwitin);
 			// CONTACT INFO
 			var email = response.responseJSON.contact_info.email;
 			var phone = response.responseJSON.contact_info.phone;
@@ -84,10 +85,10 @@ $(document).ready(function() {
 			+ response.responseJSON.contact_info.street_address.state + ", "
 			+ response.responseJSON.contact_info.street_address.zip_code;
 			var contactinfo = email + '<hr>' + phone + '<hr>' + address;
-			$('.body2').html(contactinfo);
+			$('.body5').html(contactinfo);
 	//SCHOOL1
 			var school1 = response.responseJSON.schools[0].name;
-			$('.three header').html(school1);
+			$('.body1 h1').html(school1);
 			var degree = response.responseJSON.schools[0].degree;
 			var gpa = response.responseJSON.schools[0].gpa;
 			var gpadegree = degree + ', ' + gpa + ' GPA';
@@ -95,10 +96,10 @@ $(document).ready(function() {
 			var minor = response.responseJSON.schools[0].minor;
 			var startend = response.responseJSON.schools[0].start_month_year + ' - ' + response.responseJSON.schools[0].end_month_year;
 			var school1info = gpadegree + '<hr>' + major + ', ' + minor + '<hr>' + startend;
-			$('.body3 h1').html(school1info);
+			$('.body1 p').html(school1info);
 			// Experience
 			var organization = response.responseJSON.experience[0].organization;
-			$('.four header').html(organization);
+			$('.body2 h1').html(organization);
 			var project = response.responseJSON.experience[0].project;
 			var role = response.responseJSON.experience[0].role;
 			var projrole = project + ', ' + role;
@@ -113,22 +114,22 @@ $(document).ready(function() {
 			var res4 = response.responseJSON.experience[0].responsibilities[4];
 			var responsibilities = res0 + '<br>' + res1 + '<br>' + res2 + '<br>' + res3 + '<br>' + res4;
 			var experience1 = projrole + '<hr>' + locstartend + '<hr>';
-			$('.body4 h1').html(experience1);
-			$('.body4 p').html(responsibilities);
+			$('.body2 h2').html(experience1);
+			$('.body2 p').html(responsibilities);
 			//SKILLS
 			var title = response.responseJSON.skill[0].title;
 			var yearsexp = response.responseJSON.skill[0].experience;
 			var category = response.responseJSON.skill[0].category;
 			var catyear = category + ', ' + yearsexp;
 			var skillnameexp = title + '<hr>' + catyear + ' years experience.' ;
-			$('.body5 h1').html(skillnameexp);
+			$('.body3 h1').html(skillnameexp);
 			//ACCOMPLISHMENTS
 			var accname = response.responseJSON.accomplishments[0].title;
 			var accdate = response.responseJSON.accomplishments[0].month_year;
 			var description = response.responseJSON.accomplishments[0].description;
 			var acc1 = accname + '<hr>' + accdate + '<hr>' + description;
-			$('.body6 h1').html(accname + ', ' + accdate);
-			$('.body6 p').html(description);
+			$('.body4 h1').html(accname + ', ' + accdate);
+			$('.body4 p').html(description);
 			}
 			
 
