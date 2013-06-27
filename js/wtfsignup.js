fdbc81dbb0b30d21d6f374cd51f3263cf4a6700f
Nathan Glass <nglass1988@gmail.com>
2013-06-27 16:43:33 -0700

    just fixed responsibilities add

--- js/wtfsignup.js
+++ js/wtfsignup.js
@@ -1,54 +1,98 @@
 
+	function addRes(){
+	var whatev = $('#responsibilities').children().first().clone();
+	$('.zargon7').before(whatev);
+
+	return false;
+	}
+	function addExp(){
+		var grab = $(this).closest('.multiple').children().not('.add_new' ).first().clone();
+		var whatever = grab.find('.zargon7');
+		whatever.click(addRes);
+		$(this).parent().parent().before(grab);
+
+		return false;
+	}
+
 	$(document).ready(function() {
+
 	$('.bxwrap').css('left', '0')
 	
-	//hover event
-	//click event to add the option to add school, add more credentials
-	$('.add_new a').click(function() {
-
-	  var grab = $(this).closest('.multiple').children().not('.add_new').first().clone();
-
-	  $(this).parent().parent().before(grab);
-
-	  return false;
-	});
-	//submit function
+	
+	$('.add_new a').click(addExp); 
+	 
+	$('.responsies a').click(addRes);
+	
 	$('#submit').submit(function() {
 		var userData = {};
 		
 		// contact_info
 		userData.contact_info 							= [];
-		userData.contact_info.email 					= $('#email').val();
-		userData.contact_info.phone						= $('#phone').val();
+		userData.contact_info.email 					= $('.email').val();
+		userData.contact_info.phone						= $('.phone').val();
 		// street_address
 		userData.contact_info.street_address 			= [];
-		userData.contact_info.street_address.city 		= $('#city').val();
-		userData.contact_info.street_address.state 		= $('#state').val();
-		userData.contact_info.street_address.zip_code	= $('#zip_code').val();
-		userData.contact_info.street_address.street		= $('#street').val();
+		userData.contact_info.street_address.city 		= $('.city').val();
+		userData.contact_info.street_address.state 		= $('.state').val();
+		userData.contact_info.street_address.zip_code	= $('.zip_code').val();
+		userData.contact_info.street_address.street		= $('.street').val();
 		
 		// name, twitter, website, linked_in
-		userData.name_first 							= $('#name_first').val();
-		userData.name_last								= $('#name_last').val();
-		userData.website								= $('#website').val();
-		userData.twitter 								= $('#twitter').val();
-		userData.linked_in  							= $('#linked_in').val();	
+		userData.name_first 							= $('.name_first').val();
+		userData.name_last								= $('.name_last').val();
+		userData.website								= $('.website').val();
+		userData.twitter 								= $('.twitter').val();
+		userData.linked_in  							= $('.linked_in').val();	
 		//school groups
 		userData.schools 								= [];
-
+		$('.cont1').each(function() {
+			var end 									= $(this).find('.end_month_year').val();
+			var end_month_year							= end.slice(5,7) + end.slice(2,4);
+			var start 									= $(this).find('.start_month_year').val();
+			var start_month_year						= start.slice(5,7) + start.slice(2,4);
+			userData.schools.push({
+				name : $(this).find('.name').val(),
+				major : $(this).find('.major').val(),
+				minor : $(this).find('.minor').val(),
+				start_month_year : start_month_year,
+				end_month_year : end_month_year,
+				gpa : $(this).find('.gpa').val()	
+			});
+		
+		});
+		//experience
+		userData.experience								= [];
+		$('.body2').each(function() {
+			var end 									= $(this).find('.end_month_year').val();
+			var end_month_year							= end.slice(5,7) + end.slice(2,4);
+			var start 									= $(this).find('.start_month_year').val();
+			var start_month_year						= start.slice(5,7) + start.slice(2,4);
+			userData.experience.push({
+				organization : $(this).find('.organization').val(),
+				project : $(this).find('.project').val(),
+				role : $(this).find('.role').val(),
+				location : $(this).find('.location').val(),
+				start_month_year : start_month_year,
+				end_month_year : end_month_year,
+			});
 
 
 		console.log(userData);
-		
 		return false;
+		});
+		//skills
+		userData.skills 								= [];
+		$('')
+
 	});
+		
+
 
 
 
 	//figure out how to prevent this function from interferring with mouseenter before adding a call to it
 	var i = 0;
 	function cycle() {
-
 		setTimeout(function() {
 		var prev = $('#info div:nth-of-type('+(i-1)+')');
 		var current = $('#info div:nth-of-type('+i+')');
@@ -89,6 +133,7 @@
 		hideandshow();
 	}); //closes mouseenter function
 });
+
 
 
 
